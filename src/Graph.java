bd061666348c4b2c0f556be8515e166958bd6bac
Alex Kovalyov <alleexx6@gmail.com>
2020-10-03 10:14:40 +0300

    Graph New project

---
+++ src/Graph.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Graph {
+
+   private LinkedList graph;
+
+   public  Graph(int[][] graphMat){
+       graph= convert(graphMat);
+
+   }
+
+   private LinkedList convert(int [][]Grapg){
+
+       return
+   }
+}
